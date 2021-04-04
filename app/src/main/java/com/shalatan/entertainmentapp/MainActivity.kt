4626c2df35cd6da4e0e01f90b18a28ad25b54af2
Shashank Singh <shashanksingh6688@gmail.com>
2021-04-05 01:01:05 +0530

    Cleaned Code, Removed unused classes and resources

--- app/src/main/java/com/shalatan/entertainmentapp/MainActivity.kt
+++ app/src/main/java/com/shalatan/entertainmentapp/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
