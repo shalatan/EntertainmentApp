5693119d6ddfc7926d77ccf9c7550e1d1846d7e3
Shashank Singh <shashanksingh6688@gmail.com>
2021-04-13 04:49:04 +0530

    doc : added more info

--- app/src/main/java/com/shalatan/entertainmentapp/utils/Constants.kt
+++ app/src/main/java/com/shalatan/entertainmentapp/utils/Constants.kt
@@ -1,13 +1,14 @@
 package com.shalatan.entertainmentapp.utils
 
+import com.shalatan.entertainmentapp.BuildConfig
+
 object Constants {
     const val REGION_INDIA = "IN"
     const val BASE_URL = "https://api.themoviedb.org/"
-    const val API_KEY = "ea9a49ebf2b74721a75aae271ebd3036"
+    const val API_KEY = BuildConfig.TMDB_API
     const val IMG_BASE_URL = "https://image.tmdb.org/t/p/w500"
     const val IMG_BASE_URL_O = "https://image.tmdb.org/t/p/original"
     const val VIR = "videos,images,reviews,credits"
-    const val YOUTUBE_API_KEY = "AIzaSyACUufzXxvSgLwqxxHHJkNnEkxGqJHpx1U"
 
 //    1
 //    implementation 'androidx.palette:palette-ktx:1.0.0'
