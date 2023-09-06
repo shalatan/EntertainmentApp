617dd205315cc5c93bf4bd5d3ded0aa2dd9415d2
shashank-dropshop <44257541+shalatan@users.noreply.github.com>
2023-09-06 21:05:50 +0530

    updated dependencies: gradle to 8.0 compilesdk to 34 kotlin 1.10

--- app/src/main/java/com/shalatan/entertainmentapp/database/MovieDAO.kt
+++ app/src/main/java/com/shalatan/entertainmentapp/database/MovieDAO.kt
@@ -7,16 +7,16 @@ import androidx.room.*
 interface MovieDAO {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(savedMovie: SavedMovie)
+    fun insert(savedMovie: SavedMovie)
 
     @Update
-    suspend fun update(savedMovie: SavedMovie)
+    fun update(savedMovie: SavedMovie)
 
     @Query("SELECT * FROM saved_movies_table WHERE isRated = 1")
     fun getAllRatedMovies(): LiveData<List<SavedMovie>>
 
     @Query("SELECT * FROM saved_movies_table WHERE isRated = 1")
-    suspend fun getAllRatedMoviesList(): List<SavedMovie>
+     fun getAllRatedMoviesList(): List<SavedMovie>
 
     @Query("SELECT * FROM saved_movies_table WHERE isWatchLater = 1")
     fun getAllWatchLaterMovies(): LiveData<List<SavedMovie>>
@@ -26,29 +26,29 @@ interface MovieDAO {
     fun getAllRecommendedMovies(): LiveData<List<SavedMovie>>
 
     @Query("DELETE FROM saved_movies_table WHERE isRated = 0 AND isWatchLater = 0")
-    suspend fun clearResidueMoviesFromDatabase()
+     fun clearResidueMoviesFromDatabase()
 
     @Query("UPDATE saved_movies_table SET recommendationWeight = 0 WHERE isWatchLater = 1")
-    suspend fun clearRecommendationWeightOfWatchLaterMovies()
+    fun clearRecommendationWeightOfWatchLaterMovies()
 
     @Query("UPDATE saved_movies_table SET isWatchLater = :isWatchLater WHERE Id = :id")
-    suspend fun changeWatchLaterStatus(id: Int, isWatchLater: Boolean)
+    fun changeWatchLaterStatus(id: Int, isWatchLater: Boolean)
 
     @Query("UPDATE saved_movies_table SET isRated = :isRated, rating = :rating WHERE Id = :id")
-    suspend fun changeRatedStatus(id: Int, isRated: Boolean, rating: Float)
+    fun changeRatedStatus(id: Int, isRated: Boolean, rating: Float)
 
     @Query("UPDATE saved_movies_table SET isRecommendationConsidered = :isRecommendationConsidered WHERE Id = :id")
-    suspend fun changeRecommendationConsideredStatus(id: Int, isRecommendationConsidered: Boolean)
+    fun changeRecommendationConsideredStatus(id: Int, isRecommendationConsidered: Boolean)
 
     @Query("UPDATE saved_movies_table SET recommendationWeight = recommendationWeight+:rating WHERE ID = :id")
-    suspend fun updateMovieRecommendationWeight(id: Int, rating: Float)
+    fun updateMovieRecommendationWeight(id: Int, rating: Float)
 
     @Delete
-    suspend fun delete(savedMovie: SavedMovie)
+    fun delete(savedMovie: SavedMovie)
 
     @Query("SELECT count(*)!=0 FROM saved_movies_table WHERE Id = :uid AND isRated = 1")
-    suspend fun isMovieInRatedList(uid: Int): Int
+    fun isMovieInRatedList(uid: Int): Int
 
     @Query("SELECT count(*)!=0 FROM saved_movies_table WHERE Id = :uid AND isWatchLater = 1")
-    suspend fun isMovieInWatchLaterList(uid: Int): Int
+    fun isMovieInWatchLaterList(uid: Int): Int
 }
